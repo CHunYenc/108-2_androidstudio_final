9270383aa6dc2df63e951ced182e69f51edc6174
CHunYenc <yuhnoesty0402@hotmail.com.tw>
2020-06-22 01:26:33 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
-from flask import Flask , Blueprint
+from . import views
+from .api import api as api_blueprint
+from .models import Users
+from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -8,16 +11,11 @@ if app.config["ENV"] == "production":
 else:
     app.config.from_object("config.DevelopmentConfig")
 
-print(f'ENV is set to: {app.config["ENV"]}')
-
 # Models 關於資料庫
 db = SQLAlchemy(app)
 Migrate = Migrate(app, db)
 # db
-from .models import Users
 # Views And Router
-from .api import api as api_blueprint
 # # API
-app.register_blueprint(api_blueprint , url_prefix='/api')
+app.register_blueprint(api_blueprint, url_prefix='/api')
 # # Access
-from . import views
